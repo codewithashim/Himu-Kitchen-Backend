fdbe1d74f9b99f1bc590d8348acc9f9ce3ed2d73
codewithashim <codewithashim@gmail.com>
2022-11-08 09:31:16 +0600

    jwt veryfiy added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 5000;
 const cors = require("cors");
 require("dotenv").config();
 const colors = require("colors");
+const jwt = require("jsonwebtoken");
+const { json } = require("express");
 
 app.use(cors());
 app.use(express.json());
@@ -32,7 +34,54 @@ dbConnect();
 
 // ======================= MongoDB Connection =======================
 
+// ======================= JWT Token =======================
+
+//========================= verify token =============================
+
+function verifyTokenJwt(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({
+      success: false,
+      message: "Unauthorized access",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(401).send({
+        success: false,
+        message: "unauthorized access",
+      });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+// ======================= JWT Token =======================
+
 // ======================= Middleware ===============================
+
+app.post("/jwt", (req, res) => {
+  try {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.MAIN_JWT_SECRIPT_KEY);
+
+    res.send({
+      success: true,
+      message: "Successfully got the data from jwt",
+      token,
+    });
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
 
 // ======================= Middleware ===============================
 
